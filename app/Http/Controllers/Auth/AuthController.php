9c3961fa84ef32945b5481da2bb0b0b878aff475
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2017-03-21 16:24:09 +0700

    Authcontroller

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,10 +2,51 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Exceptions\NoActiveAccountException;
+
+use App\Http\AuthTraits\Social\ManagesSocialAuth;
+
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+use App\Http\Requests;
+
+use Socialite;
 
 class AuthController extends Controller
 {
-    //
+    use AuthenticatesUsers,ManagesSocialAuth;
+    protected $redirectTo = '/';
+    public function __construct(){
+    	$this->middleware('guest', ['except' => ['logout',
+
+            'redirectToProvider',
+
+            'handleProviderCallback']
+
+        ]);
+    }
+    public function checkStatusLevel(){
+    	if( ! Auth::user()->isActiveStatus()){
+    		Auth::logout();
+    		throw new  NoActiveAccountException;
+    		
+    	}
+    }
+    public function redirectPath(){
+    	if(Auth::user()->isAdmin()){
+    		return 'admin';
+    	}
+    	return property_exists($this, 'redirectTo')? $this->redirectTo : '/';
+
+    }
+    /**
+	* Handle a login request to the application.
+	*
+	* @param \Illuminate\Http\Request $request
+	* @return \Illuminate\Http\Response
+	*/
 }
