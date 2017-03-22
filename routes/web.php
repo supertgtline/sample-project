b1859e63c93e077b4069bedc9f0c53a38869c39f
thaogiang.90@gmail.com <thaogiang.90@gmail.com>
2017-03-22 23:11:24 +0700

    Social full

--- routes/web.php
+++ routes/web.php
@@ -10,28 +10,51 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('test', 'TestController@index')->middleware(['auth', 'throttle']);
-Auth::routes();
+;
 
 Route::get('/admin','AdminController@index')->name('admin');
+// Authentication routes
 
-Route::get('terms-of-service', 'PagesController@terms');
+Route::get('login', 'Auth\AuthController@showLoginForm')->name('login');
 
-Route::get('privacy','PagesController@privacy');
+Route::post('login', 'Auth\AuthController@login');
 
-Route::get('/', 'PagesController@index');
-Route::get('widget/create', 'WidgetController@create')->name('widget.create');
-
-Route::get('widget/{widget}-{slug?}','WidgetController@show')->name('widget.show');
-Route::resource('widget', 'WidgetController', ['except' => ['show', 'create']]);
-Route::patch('widget/{widget}', 'WidgetController@update');
-
-// socialite routes
-// 
-Route::get('auth/{provider}','Auth\AuthController@uthController@redirectToProvider');
-
-Route::get('auth/{provider}/callback','Auth\AuthController@handleProviderCallback');
+Route::post('logout', 'Auth\AuthController@logout')->name('logout');
+// home page route
 
 Route::get('/', 'PagesController@index')->name('home');
+// Password routes
+
+
+// Password Reset Routes...
+
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+// Privacy route
+
+Route::get('privacy', 'PagesController@privacy');
+// Registration routes
+Route::get('register', 'Auth\AuthController@showRegistrationForm')->name('register');
+
+Route::post('register', 'Auth\AuthController@register');
+// Socialite routes
+
+Route::get('auth/{provider}', 'Auth\AuthController@redirectToProvider');
+Route::get('auth/{provider}/callback', 'Auth\AuthController@handleProviderCallback');
+// Terms route
+
+Route::get('terms-of-service', 'PagesController@terms');
+// test route
+Route::get('test', 'TestController@index')->middleware(['auth', 'throttle']);
+
+// Widget routes
+
+Route::get('widget/create',  'WidgetController@create')->name('widget.create');
+Route::get('widget/{id}-{slug?}', 'WidgetController@show')->name('widget.show');
+Route::resource('widget', 'WidgetController', ['except' => ['show', 'create']]);
 
